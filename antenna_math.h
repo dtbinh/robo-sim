eed460a90670a3acbb60fb871701622fcd902fb8
charlesdrotar <drotarcharles@yahoo.com>
2013-03-15 12:59:51 -0700

    Update antenna_math.h
    changed euler2Rot from static 

--- antenna_math.h
+++ antenna_math.h
@@ -1,6 +1,7 @@
 void matMatMult( const float a_mat[3][4], const float b_mat[3][4], float c_mat[3][4] );
 void matInverse( const float a_mat[3][4], float a_inv_mat[3][4] );
-static void euler2Rot( const int rot_vec[3], float rot_mat[3][3] );
 void cartToSpher( const float cart_vec[3], float polar_vec[3] );
 int nearestGapDeg( float degrees, int gap );
 void buildMat( const float pos_vec[3], const int rot_vec[3], float mat[3][4] );
+//convert back to static type when done with debugging
+void euler2Rot( const int rot_vec[3], float rot_mat[3][3] );
