6774c8c32e64008758b983cd471a36d1afdddec3
charlesdrotar <drotarcharles@yahoo.com>
2013-03-15 07:51:37 -0700

    Create Mymath.h
    Header file for Mymath.c, which is used to import functions into Main.c

---
+++ Mymath.h
@@ -0,0 +1,6 @@
+float cosDegrees(float degrees);
+float sinDegrees(float degrees); 
+float tanDegrees(float degrees); 
+float acosDegrees(float value); 
+float asinDegrees(float value); 
+float atanDegrees(float value);  
