dd116eec24e4f3fb7df72e54abbfc2cb680074d9
charlesdrotar <drotarcharles@yahoo.com>
2013-03-15 23:25:09 -0700

    Update antenna.c
    changed functions to lower case "camel" convention.

--- antenna.c
+++ antenna.c
@@ -11,18 +11,18 @@ static void relativeVectors( const float 1a_mat[3][4], const float 1v_mat[3][4],
 	// calculating i_ant_1 * i_robot_1 * robot_2 * ant_2 yields ant_2 from the
 	// coordinate system of ant_1
 	
-	MatInverse(1a_mat, t1_mat);
-	MatInverse(1v_mat, t2_mat);
+	matInverse(1a_mat, t1_mat);
+	matInverse(1v_mat, t2_mat);
 	
-	MatMatMult(t1_mat, t2_mat, t3_mat);
-	MatMatMult(t3_mat, 2v_mat, t1_mat);
-	MatMatMult(t1_mat, 2a_mat, t3_mat);		// t3_Mat is now ant_2 from ant_1
+	matMatMult(t1_mat, t2_mat, t3_mat);
+	matMatMult(t3_mat, 2v_mat, t1_mat);
+	matMatMult(t1_mat, 2a_mat, t3_mat);		// t3_Mat is now ant_2 from ant_1
 	
 	2a_from_1a[0] = t3_mat[0][3];
 	2a_from_1a[1] = t3_mat[1][3];
 	2a_from_1a[2] = t3_mat[2][3];
 	
-	MatInverse(t3_mat, t1_mat);				// inverting to get ant_1 from ant_2's perspective
+	matInverse(t3_mat, t1_mat);				// inverting to get ant_1 from ant_2's perspective
 	
 	1a_from_2a[0] = t1_mat[0][3];
 	1a_from_2a[1] = t1_mat[1][3];
