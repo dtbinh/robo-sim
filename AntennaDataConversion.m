6903cd400a1fbadf1fc1c4b3850b6ec4fb13066f
charlesdrotar <drotarcharles@yahoo.com>
2013-04-02 01:27:39 -0500

    Update AntennaDataConversion.m
    Last two lines are the output. All intermediate plots and calculations are purely for evaluation purposes. The plot in question is "True Radiation Pattern" and it should be very similar to the polar plot when oriented correctly in the X-Y plane. 

--- AntennaDataConversion.m
+++ AntennaDataConversion.m
@@ -1,36 +1,120 @@
+%{ 
+/////////////
+PROGRAM NOTES
+/////////////
+
+To Determine AntennaData
+------------------------
+1)Name of file
+2)Name of spreadsheet
+3)Data Range
+
+To Determine RotAngle 
+----------------
+1)Rotate the Polar plot so the main lobe is centered at 0 degrees
+*This will require verification using the polar plot
+2)RotAngle = 0 - Main_Lobe_ANGLE 
+3)This will effectively orient the main lobe at 0 degrees and prep the plot
+for revolution about the axis
+
+OR
+
+1)Take Degree measurement for "Max dB" from Antenna measurement sheet.
+%}
+
 clc, clear all, close all;
-% Polar Data
+
+% Read Spreadsheet
     AntennaData = xlsread('data2.xlsx','Trace Data','A5:C41'); %Read data from file  
     Angle_Position =(pi/180)*AntennaData(:,1); %0,5,10,....180.
     Lin_Mag = AntennaData(:,2); % radius
-    Phase = AntennaData(:,3); % phase 
-    
-    figure(1)
-    subplot(1,2,1)                  %Figure1, Plot1
+
+% Variables
+    RotAngle = -95; 
+    phi = -pi:.01:pi;  
+    phi_slice = -pi:2*pi:pi;
+
+% Rectangular Data Unmodified
+    Xp = Lin_Mag.*cos(Angle_Position);      %Convert polar to Cartesian X value
+    Yp = Lin_Mag.*sin(Angle_Position);      %Convert polar to Cartesian Y value
+
+% Rectangular Data Modified
+    Xr = Xp .* cosd(RotAngle) - Yp .* sind(RotAngle); %Modified X Coordinate
+    Yr = Xp .* sind(RotAngle) + Yp .* cosd(RotAngle); %Modified Y Coordinate
+
+%% POLAR PLOT
+% Generate Subplot of Polar and Cartesian(Modified and Unmodified)   
+    figure('name','Polar Plot','numbertitle','off')
     polar(Angle_Position, Lin_Mag); %2D polar plot
     
-% Rectangular Data
-    Xp = Lin_Mag.*cosd(Phase);      %Convert polar to Cartesian X value
-    Yp = Lin_Mag.*sind(Phase);      %Convert polar to Cartesian Y value
+%% UNMODIFIED RADIATION PATTERN
+
+% Plot of Rectangular Data Unmodified    
+    figure('name','Radiation Unmodified','numbertitle','off')
+    subplot(1,3,1)
+    plot(Xp,Yp)                
     
- % 3D points of the surface
-    phi = -pi:.01:pi;               %Revolution of 2D to get 3D plot,increment => # of data
+% 3D Surface of Revolution Unmodified Slice  
+    X = repmat(Xp,size(phi_slice));   %X coordinate
+    Y = Yp * cos(phi_slice);          %Y coordinate
+    Z = Yp * sin(phi_slice);          %Z coordinate
+    subplot(1,3,2)
+    mesh(X,Y,Z, 'FaceColor','interp','FaceLighting','phong'); %3D plot
+    camlight right %Lighting Effect, not really necessary
+    xlabel('X', 'Color', 'b','FontSize',20)
+    ylabel('Y', 'Color', 'b','FontSize',20)
+    zlabel('Z', 'Color', 'b','FontSize',20)
+    
+% 3D Surface of Revolution Unmodified           
     X = repmat(Xp,size(phi));       %X coordinate
     Y = Yp * cos(phi);              %Y coordinate
     Z = Yp * sin(phi);              %Z coordinate
-    
-    subplot(1,2,2)%Figure1, Plot2
+    subplot(1,3,3)
     mesh(X,Y,Z, 'FaceColor','interp','FaceLighting','phong'); %3D Radiation plot
     camlight right %Lighting Effect, not really necessary
- 
-    %% Figure2 is used purely for reference/diagnosis of Figure1, Plot2
-    figure(2)                   
-    subplot(1,2,1)              % Figure2, Plot1
-    plot(Xp,Yp)                 %image to be rotated to generate Figure1, Plot2
-    phi = -pi:2*pi:pi;          %single 3D representation of plot(Xp,Yp)
-    X = repmat(Xp,size(phi));   %X coordinate
-    Y = Yp * cos(phi);          %Y coordinate
-    Z = Yp * sin(phi);          %Z coordinate
-    subplot(1,2,2)              % Figure2, Plot2
+    xlabel('X', 'Color', 'r','FontSize',20)
+    ylabel('Y', 'Color', 'r','FontSize',20)
+    zlabel('Z', 'Color', 'r','FontSize',20)
+
+%% MODIFIED RADIATION PATTERN    
+
+% Plot of Rectangular Data Modified    
+    figure('name','Radiation Rotated','numbertitle','off')
+    subplot(1,3,1)
+    plot(Xr,Yr)
+    
+% 3D Surface of Revolution Modified Slice   
+    X = repmat(Xr,size(phi_slice));   %X coordinate
+    Y = Yr * cos(phi_slice);          %Y coordinate
+    Z = Yr * sin(phi_slice);          %Z coordinate
+    subplot(1,3,2)
     mesh(X,Y,Z, 'FaceColor','interp','FaceLighting','phong'); %3D plot
     camlight right %Lighting Effect, not really necessary
+    xlabel('X', 'Color', 'b','FontSize',20)
+    ylabel('Y', 'Color', 'b','FontSize',20)
+    zlabel('Z', 'Color', 'b','FontSize',20)
+    
+% 3D Surface of Revolution Modified 
+    X = repmat(Xr,size(phi));   %X coordinate
+    Y = Yr * cos(phi);          %Y coordinate
+    Z = Yr * sin(phi);          %Z coordinate
+    subplot(1,3,3)
+    mesh(X,Y,Z, 'FaceColor','interp','FaceLighting','phong'); %3D plot
+    camlight right %Lighting Effect, not really necessary
+    xlabel('X', 'Color', 'r','FontSize',20)
+    ylabel('Y', 'Color', 'r','FontSize',20)
+    zlabel('Z', 'Color', 'r','FontSize',20)
+    
+%% RECTANGULAR COORDINATES ROTATED BACK TO ORIGINAL ORIENTATION
+    Xoriginal = X*cosd(-RotAngle) - Y*sind(-RotAngle);
+    Yoriginal = X*sind(-RotAngle) + Y*cosd(-RotAngle);
+    Zoriginal = Z;
+    figure('name','True Radiation Pattern','numbertitle','off')
+    mesh(Xoriginal,Yoriginal,Zoriginal, 'FaceColor','interp','FaceLighting','phong'); %3D plot
+    camlight right %Lighting Effect, not really necessary
+    xlabel('X', 'Color', 'r','FontSize',20)
+    ylabel('Y', 'Color', 'r','FontSize',20)
+    zlabel('Z', 'Color', 'r','FontSize',20)
+
+%% RECTANGULAR COORDINATES TO SPHERICAL   
+    [AZIMUTH,ELEVATION,RADIUS] = cart2sph(Xoriginal,Yoriginal,Zoriginal);
